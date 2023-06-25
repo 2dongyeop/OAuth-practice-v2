1b6bcd8b528f2b7c94db03143de6adf7752ab3f7
leedongyeop <ldy_1204@naver.com>
2023-06-25 16:04:38 +0900

    Inital commit

---
+++ src/main/java/com/example/oauthpracticev2/OauthPracticeV2Application.java
@@ -0,0 +1,13 @@
+package com.example.oauthpracticev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthPracticeV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthPracticeV2Application.class, args);
+    }
+
+}
