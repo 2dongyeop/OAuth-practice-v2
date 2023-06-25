173644963d778136fcb143dde7d086dbd1685445
leedongyeop <ldy_1204@naver.com>
2023-06-25 16:47:51 +0900

    feat : 값을 바인딩 할 프로퍼티와 설정파일 추가
    OauthProperties는 `@ConfigurationProperties(prefix = "oauth2")`를 이용해 application-oauth.yml의 값을 바인딩할 수 있는 상태로 만듭니다.
    
    oauth2 하위에 있는 user와 provider를 바인딩할 수 있도록 프로퍼티 클래스 내에 static class로 작성합니다.
    
    OauthConfig 클래스를 작성할 때는, `@EnableConfigurationProperties(OauthProperties.class)`를 작성하여 해당 프로퍼티 클래스가 yml 파일의 값을 실제로 받아올 수 있도록 설정을 합니다.

---
+++ src/main/java/com/example/oauthpracticev2/global/oauth/OauthConfig.java
@@ -0,0 +1,16 @@
+package com.example.oauthpracticev2.global.oauth;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(OauthProperties.class)
+public class OauthConfig {
+
+    private final OauthProperties properties;
+
+    public OauthConfig(OauthProperties properties) {
+        this.properties = properties;
+    }
+
+}
